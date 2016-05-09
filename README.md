bbf0acf83cd1a5536e89189f677e5fda3d60478f
Christian Blaschke <mail@platdesign.de>
2016-05-09 18:58:29 +0200

    init

---
+++ README.md
@@ -0,0 +1,61 @@
+#mongoose-i18n
+
+Mongoose plugin for internationalization.
+
+
+
+#Install
+
+`npm install --save i18ngoose`
+
+
+#Usage
+
+```javascript
+
+const mongoose = require('mongoose');
+const i18ngoose = require('i18ngoose');
+
+// Create schema and define which attribute should be 'i18nable'
+let schema = new mongoose.Schema({
+	title: {
+		type: String,
+		i18n: true
+	}
+});
+
+// Load plugin to schema and define languages
+schema.plugin(i18ngoose, {
+	languages: ['de', 'en', 'tr']
+});
+
+// Create mongoose model constrictor
+const Model = mongoose.model('TestModel', schema);
+
+// Init new document from Model for given language
+let model = Model.i18nInit('en', {
+	title: 'Hello'
+});
+
+// Set attributes for given language
+model.i18nSet('de', {
+	title: 'Hallo'
+});
+
+// work with the model (save, ...)
+model.save().then(...);
+
+// get translated object
+let de = model.toObjectTranslated({ translation: 'de' });
+let en = model.toObjectTranslated({ translation: 'en' });
+
+// get translated json-object
+let de = model.toJSONTranslated({ translation: 'de' });
+let en = model.toJSONTranslated({ translation: 'en' });
+```
+
+
+
+#Author
+
+Christian Blaschke <mail@platdesign.de>
---
+++ test/unit.js
@@ -0,0 +1,131 @@
+'use strict';
+
+const Code = require('code');
+const expect = Code.expect;
+
+
+
+const mongoose = require('mongoose');
+const i18ngoose = require('../');
+
+
+
+
+describe('i18n', () => {
+
+	let Model;
+
+	before(() => {
+
+		let schema = new mongoose.Schema({
+			title: {
+				type: String,
+				i18n: true
+			},
+			story: {
+				type: String,
+				i18n: true
+			},
+			sub: {
+				test: {
+					type: String,
+					i18n: true
+				}
+			},
+			huhu: {
+				type: String
+			}
+		})
+
+		schema.plugin(i18ngoose, {
+			languages: ['de', 'en', 'tr']
+		});
+
+		Model = mongoose.model('testmodel', schema);
+
+	});
+
+
+	let rawDoc = {
+		title: 'title',
+		story: 'story',
+		sub: {
+			test: 'test'
+		},
+		huhu: 'huhu'
+	};
+
+
+
+	describe('Model creation', () => {
+
+		testForIso('de');
+		testForIso('en');
+		testForIso('tr');
+
+		function testForIso(iso) {
+
+			it(`should have attributes with iso: ${iso}`, () => {
+
+				let model = Model.i18nInit(iso, rawDoc);
+
+				expect(model.title[iso]).to.be.a.string();
+				expect(model.title[iso]).to.equal(rawDoc.title);
+
+				expect(model.story[iso]).to.be.a.string();
+				expect(model.story[iso]).to.equal(rawDoc.story);
+
+				expect(model.sub.test[iso]).to.be.a.string();
+				expect(model.sub.test[iso]).to.equal(rawDoc.sub.test);
+
+				expect(model.huhu).to.be.a.string();
+				expect(model.huhu).to.equal(rawDoc.huhu);
+
+
+				let translated = model.toObjectTranslated({ translation: iso });
+
+				expect(translated.title).to.equal(rawDoc.title);
+				expect(translated.story).to.equal(rawDoc.story);
+				expect(translated.sub.test).to.equal(rawDoc.sub.test);
+				expect(translated.huhu).to.equal(rawDoc.huhu);
+
+				let json = model.toJSONTranslated({ translation: iso });
+
+				expect(json.title).to.equal(rawDoc.title);
+				expect(json.story).to.equal(rawDoc.story);
+				expect(json.sub.test).to.equal(rawDoc.sub.test);
+				expect(json.huhu).to.equal(rawDoc.huhu);
+			});
+		}
+
+	})
+
+
+	describe('Model setting attributes', () => {
+
+		it('should set correct second iso', function() {
+
+			let rawDoc = {
+				title: 'title',
+				story: 'story',
+				sub: {
+					test: 'test'
+				},
+				huhu: 'huhu'
+			};
+
+			let model = Model.i18nInit('de', rawDoc);
+
+			model.i18nSet('en', { title: '123' });
+
+			expect(model.title.de).to.equal(rawDoc.title);
+			expect(model.title.en).to.equal('123');
+
+		});
+
+	});
+
+
+
+
+})
